e8def1f80c07b7393c149f12d85c872b0264498c
juliuscaesar169 <juliocsanchez169@gmail.com>
2023-02-20 23:37:28 +0100

    add About section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import About from '@/components/About';
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import type { NextPage } from 'next';
@@ -20,6 +21,9 @@ const Home: NextPage = () => {
       </section>
 
       {/* About */}
+      <section id="about" className="snap-center">
+        <About />
+      </section>
 
       {/* Experience */}
 
