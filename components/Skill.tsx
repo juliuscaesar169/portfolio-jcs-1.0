22c112add7c8da7290e30a191300f5d2317b9a35
juliuscaesar169 <juliocsanchez169@gmail.com>
2023-02-21 10:50:45 +0100

    Small Fix

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -7,14 +7,16 @@ type Props = {
 
 const Skill = ({ directionLeft }: Props) => {
   return (
-    <div className="group relative flex cursor-pointer">
-      <motion.img
-        initial={{
-          x: directionLeft ? -200 : 200,
-          opacity: 0,
-        }}
-        transition={{ duration: 1 }}
-        whileInView={{ opacity: 1, x: 0 }}
+    <motion.div 
+      initial={{
+        x: directionLeft ? -200 : 200,
+        opacity: 0,
+      }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      className="group relative flex cursor-pointer"
+    >
+      <img
         src="https://cdn-icons-png.flaticon.com/512/5968/5968381.png"
         alt="Skill Icon"
         className="rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
@@ -24,7 +26,7 @@ const Skill = ({ directionLeft }: Props) => {
           <p className="text-3xl font-bold text-black opacity-100">100%</p>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
