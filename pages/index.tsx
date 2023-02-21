6000358193fa07feb022007e2fcb07e1bc421af0
juliuscaesar169 <juliocsanchez169@gmail.com>
2023-02-21 10:29:20 +0100

    Add Skills section

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import About from '@/components/About';
 import Experience from '@/components/Experience';
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
+import Skills from '@/components/Skills';
 import type { NextPage } from 'next';
 import Head from 'next/head'
 
@@ -14,21 +15,22 @@ const Home: NextPage = () => {
 
       <Header />
 
-      {/* Hero */}
-      <section id="hero" className="snap-center">
+      <section id="hero" className="snap-start">
         <Hero />
       </section>
 
-      {/* About */}
       <section id="about" className="snap-center">
         <About />
       </section>
 
-      {/* Experience */}
       <section id="experience" className="snap-center">
         <Experience />
       </section>
+
       {/* Skills */}
+      <section id="skills" className="snap-start">
+        <Skills />
+      </section>
 
       {/* Projects */}
 
