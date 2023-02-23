894f5007ea3a00579a2366296eb04c7a94572b2f
juliuscaesar169 <juliocsanchez169@gmail.com>
2023-02-23 09:31:55 +0100

    Add Schemas

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,6 +1,8 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
+import pageInfo from './pageInfo'
+import experience from './experience'
+import project from './project'
+import skill from './skill'
+import social from './social'
 
-export const schemaTypes = [post, author, category, blockContent]
+
+export const schemaTypes = [pageInfo, experience, project, skill, social]
