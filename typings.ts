57d2b746e2468badf5e8da4331cc2047c5a1f2e6
juliuscaesar169 <juliocsanchez169@gmail.com>
2023-02-25 03:04:19 +0100

    Add typings

--- typings.ts
+++ typings.ts
@@ -1,12 +1,67 @@
-interface sanityBody {
+interface SanityBody {
     _created: string;   
     _id: string;
     _rev: string;
     _updatedAt: string;
 }
 
-export interface Social extends sanityBody {
-    _title: 'social';
-    titlte: string;
+interface Image {
+    _type: 'Image';
+    asset: {
+        _ref: string;
+        _type: 'reference';
+    };
+}
+
+export interface PageInfo extends SanityBody {
+    _type: 'pageInfo';
+    address: string;
+    backgroundInformation: Text;
+    email: string;
+    role: string;
+    heroImage: Image;
+    name: string;
+    phoneNumber: string;
+    profilePic: Image;
+}
+
+export interface Technology extends SanityBody {
+    _type: 'skill';
+    image: Image;
+    progress: number;
+    title: string;
+}
+
+export interface Skill extends SanityBody {
+    _type: 'skill';
+    image: Image;
+    progress: number;
+    title: string;
+}
+
+export interface Expetience extends SanityBody {
+    _type: 'experience',
+    company: string,
+    companyImage: Image;
+    dateStarted: Date;
+    dateEnded: Date;
+    isCurrentlyWorkingHere: boolean;
+    jobTitle: string;
+    points: string[];
+    technologies: Technology[];
+}
+
+export interface Project extends SanityBody {
+    _type: 'project';
+    title: string;
+    image: Image;
+    linkToBuild: string;
+    summary: string;
+    technologies: Technology[]
+}
+
+export interface Social extends SanityBody {
+    _type: 'social';
+    title: string;
     url: string;
 }
