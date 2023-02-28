2f10490ce4cb49b25e485d12590d7f81ed0fa6b4
juliuscaesar169 <juliocsanchez169@gmail.com>
2023-02-28 22:22:52 +0100

    Small Fix

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -16,7 +16,7 @@ const ExperienceCard = ({ experience }: Props) => {
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
         className="w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center"
-        src={urlFor(experience)?.url()}
+        src={urlFor(experience?.companyImage)?.url()}
         alt="Experience Card"
       />
 
