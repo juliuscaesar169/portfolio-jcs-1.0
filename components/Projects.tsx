ca02384d717033c0534f0dd71ccd5b9d4fd62131
juliuscaesar169 <juliocsanchez169@gmail.com>
2023-03-01 00:50:11 +0100

    Integrate Sanity Data

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { Project } from "@/typings";
+import { urlFor } from "@/sanity";
 
-type Props = {};
+type Props = {
+    projects: Project[];
+};
 
-const Projects = ({}: Props) => {
-    const projects = [1, 2, 3, 4 ,5];
+const Projects = ({ projects }: Props) => {
 
   return (
     <motion.div 
@@ -18,8 +21,8 @@ const Projects = ({}: Props) => {
         </h3>
 
         <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbarPage">
-            {projects.map((project, i) => (
-                <div key={i} className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
+            {projects?.map((project, i) => (
+                <div key={project._id} className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
                     <motion.img 
                           initial={{
                             y: -300,
@@ -28,25 +31,35 @@ const Projects = ({}: Props) => {
                           transition={{ duration: 1.2 }}
                           whileInView={{ opacity: 1, y: 0 }}
                           viewport={{once: true}}
-                        src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+                        src={urlFor(project.image)?.url()}
                         alt="Project Image"
                     />
                     <div className="space-y-10 px-0 md:px-10 max-w-6xl">
                         <h4 className="text-4xl font-semibold text-center">
                             <span className="underline decoration-[#F7AB0A]/50">
-                                Checking Project {i + 1} of {projects.length}:
+                                Project {i + 1} of {projects.length}:
                             </span>{" "}
-                            Project-Title
+                            {project.title}
                         </h4>
+
+                          {/* project's techs to show */}
+                          {/* <div className="flex items-center space-x-2 justify-center">
+                            {project.technologies?.map(technology => (
+                                <img 
+                                    key={technology._id}
+                                    className="h-10 w-10"
+                                    src={urlFor(technology?.image)?.url()}
+                                    alt="Technology Logo"
+                                />
+                            ))}
+                          </div> */}
+
                         <p className="text-lg text-center md:text-left">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor incidunt quos voluptates quam deleniti nobis, dolores commodi sint architecto a praesentium velit, nesciunt quidem nisi perferendis, neque rerum hic aliquam?
+                            {project.summary}
                         </p>
                     </div>
                 </div>
             ))}
-            {/* projects */}
-            {/* projects */}
-            {/* projects */}
         </div>
 
         <div className="w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12">
